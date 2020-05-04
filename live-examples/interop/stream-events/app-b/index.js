149a3b180ac36e5e1991715a3b357ec0cd5e7f8e
suzunov <stoyan.uzunov@tick42.com>
2020-05-04 19:00:22 +0300

    Added example for stream events handling
    Signed-off-by: suzunov <stoyan.uzunov@tick42.com>

---
+++ live-examples/interop/stream-events/app-b/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-undef */
+const APP_NAME = 'App-B';
+
+// Entry point. Initializes GlueWeb. GlueWeb instance will be attached to the global window.
+window.startApp({ appName: APP_NAME })
+  .then(() => {
+    document.getElementById("toggleStreamBtn")
+      .addEventListener('click', toggleStreamHandler);
+  })
+  .catch(console.error);
+
+let stream;
+let nextMessageId = 1;
+let intervalId;
+
+function toggleStreamHandler() {
+  // Publishing messages is active
+  if (stream == null) {
+    createStream();
+    intervalId = setInterval(publishMessage, 3000);
+
+    changeToggleButtonText('Close Stream');
+  } else {
+    clearInterval(intervalId);
+    stream.close();
+    stream = null;
+
+    changeToggleButtonText('Create Stream');
+  }
+}
+
+function subscriptionAddedHandler(subscription) {
+  const { instance } = subscription;
+  logger.info(`Subscriber "${instance.application}" added.`);
+}
+
+function subscriptionRemovedHandler(subscription) {
+  const { instance } = subscription;
+  logger.info(`Subscriber "${instance.application}" removed.`);
+}
+
+async function createStream() {
+  const methodDefinition = { name: 'G42Core.Stream.Basic' };
+  const streamOptions = {
+    subscriptionAddedHandler,
+    subscriptionRemovedHandler
+  };
+
+  try {
+    stream = await glue.interop.createStream(methodDefinition, streamOptions)
+
+    logger.info(`Stream "${methodDefinition.name}" created`);
+  } catch (error) {
+    console.error(`Failed to create stream "${methodDefinition.name}". Error: `, error);
+    logger.error(error.message || `Failed to create stream "${methodDefinition.name}"`);
+  }
+}
+
+function publishMessage() {
+  stream.push(
+    {
+      message: "Hello from publisher",
+      timeStamp: Date.now(),
+      counter: nextMessageId
+    });
+
+  nextMessageId++;
+}
+
+function changeToggleButtonText(text) {
+  const btn = document.getElementById("toggleStreamBtn");
+  btn.textContent = text;
+}
