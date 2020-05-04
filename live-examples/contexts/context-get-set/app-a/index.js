bd09e8a61d6b7fd9f84092153fc81346d465b9c9
suzunov <stoyan.uzunov@tick42.com>
2020-05-04 22:52:45 +0300

    Added example for shared context getting and setting
    Signed-off-by: suzunov <stoyan.uzunov@tick42.com>

---
+++ live-examples/contexts/context-get-set/app-a/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+const APP_NAME = 'App-A';
+
+// Entry point. Initializes GlueWeb. GlueWeb instance will be attached to the global window.
+window.startApp({ appName: APP_NAME })
+  .then(() => {
+    document.getElementById('getContextBtn')
+      .addEventListener('click', () => getContext('G42CoreContext'), false);
+  })
+  .catch(console.error);
+
+async function getContext(ctxName) {
+  try {
+    const ctxData = await glue.contexts.get(ctxName, false);
+
+    const data = Object.keys(ctxData).map((key) => ctxData[key]).join(', ');
+    logger.info(`Context "${ctxName}" has data: "${data}"`);
+  } catch (error) {
+    logger.error(error.message || `Failed to get context "${ctxName}"`);
+  }
+}
---
+++ live-examples/contexts/context-get-set/app-b/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+const APP_NAME = 'App-B';
+
+// Entry point. Initializes GlueWeb. GlueWeb instance will be attached to the window.
+window.startApp({ appName: APP_NAME })
+  .then(() => setContext('G42CoreContext', 'Glue42 Core'))
+  .then(() => {
+    document.getElementById('setContextBtn')
+      .addEventListener('click', setContextHandler, false);
+  })
+  .catch(console.error);
+
+function setContextHandler() {
+  const ctxName = document.getElementById('ctxNameInput').value;
+  const ctxData = document.getElementById('ctxDataInput').value;
+
+  setContext(ctxName, ctxData);
+}
+
+async function setContext(ctxName, ctxData) {
+  try {
+    await glue.contexts.set(ctxName, { value: ctxData })
+
+    logger.info(`Context "${ctxName}" set to "${ctxData}"`);
+  } catch (error) {
+    logger.error(error.message || `Failed to set new context data`);
+  }
+}
