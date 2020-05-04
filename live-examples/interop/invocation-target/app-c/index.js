dae38bd0114408f71f886364e725da51450366ef
suzunov <stoyan.uzunov@tick42.com>
2020-05-04 18:54:13 +0300

    Added example for method invocation target
    Signed-off-by: suzunov <stoyan.uzunov@tick42.com>

---
+++ live-examples/interop/invocation-target/app-c/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+const APP_NAME = 'App-C';
+
+// Entry point. Initializes GlueWeb. GlueWeb instance will be attached to the window.
+window.startApp({ appName: APP_NAME })
+  .then(registerGlueMethod)
+  .catch(console.error);
+
+async function registerGlueMethod() {
+  const methodDefinition = { name: 'G42Core.Basic' };
+
+  const invocationHandler = () => {
+    logger.info(`"${methodDefinition.name}" invoked.`);
+    return {
+      result: `Hello from "${APP_NAME}"`
+    };
+  };
+
+  try {
+    await glue.interop.register(methodDefinition, invocationHandler);
+
+    logger.info(`"${methodDefinition.name}" registered`);
+  } catch (error) {
+    console.error(`Failed to register "${methodDefinition.name}". Error: `, error);
+    logger.error(error.message || `Failed to register "${methodDefinition.name}"`);
+  }
+}
